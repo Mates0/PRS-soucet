3ca8ce3c4bd2662d6615c69731f4996fdd51fa77
Mates0 <matyas.vanko@educanet.cz>
2021-09-16 16:44:57 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int index = 1;
+        while(index <= 10) {
+            int input = sc.nextInt();
+            if (input % 3 == 0) {
+                if (index % 2 == 0) {
+                    sum += input;
+                }
+            }
+            index++;
+        }
+        System.out.println("Součet je:" + " " + sum);
+    }
+}
